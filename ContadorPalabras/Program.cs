c30fc9696c27a43c3164973ecd2dc0655260fe05
Fran Diaz <frandiazguitar@gmail.com>
2023-08-31 17:27:31 +0200

    Agregar archivos de proyecto.

---
+++ ContadorPalabras/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ContadorPalabras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Debe contar las palabras que contiene la cadena
+
+            string cadena = "    Hola, soy una cadena     con varias palabras    ";
+
+            int contador = 0;
+
+            cadena = Regex.Replace(cadena,@"\s+", " ").Trim();
+
+            var words = cadena.Split(' ');
+
+            contador = words.Length;
+
+            Console.WriteLine(contador);
+        }
+    }
+}
